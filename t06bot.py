479d55228cbc8f15489537e7e1a4dc3b3db08a7e
fi944 <132758667+fi944@users.noreply.github.com>
2023-06-09 10:09:44 +0300

    Update t06bot.py

--- t06bot.py
+++ t06bot.py
@@ -7,41 +7,41 @@ import asyncio
 from telethon import events
 from help import *
 c = requests.session()
-bot_username3 = '@TRS04BOT'
+bot_username = '@zmmbot'
 
 
-@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.بوت لايهم"))
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.بوت المليار"))
 async def _(event):
     if ispay[0] == "yes":
-        await event.edit('TRS04BOT')
+        await event.edit(t06bot)
     else:
         await event.edit("يجب الدفع لاستعمال هذا الامر !")
 
 
-@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع لايهم"))
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع المليار"))
 async def _(event):
     if ispay[0] == "yes":
-        await event.edit("حسنا, تأكد من انك مشترك ب قنوات الاشتراك الاجباري لتجنب الأخطأء(@JJXJXX)")
-        channel_entity = await sedthon.get_entity(bot_username3)
-        await sedthon.send_message('@TRS04BOT', '/start')
-        await asyncio.sleep(10)
-        msg0 = await sedthon.get_messages('@TRS04BOT', limit=1)
+        await event.edit("**᯽︙سيتم تجميع النقاط , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+        channel_entity = await sedthon.get_entity(bot_username)
+        await sedthon.send_message('@zmmbot', '/start')
+        await asyncio.sleep(5)
+        msg0 = await sedthon.get_messages('@zmmbot', limit=1)
         await msg0[0].click(2)
-        await asyncio.sleep(10)
-        msg1 = await sedthon.get_messages('@TRS04BOT', limit=1)
+        await asyncio.sleep(5)
+        msg1 = await sedthon.get_messages('@zmmbot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
         for i in range(100):
             if ispay[0] == 'no':
                 break
-            await asyncio.sleep(10)
+            await asyncio.sleep(5)
 
             list = await sedthon(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
             msgs = list.messages[0]
-            if msgs.message.find('(@JJXJXX)لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
-                await sedthon.send_message(event.chat_id, f"مافي قنوات بلبوت")
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await sedthon.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
                 break
             url = msgs.reply_markup.rows[0].buttons[0].url
             try:
@@ -50,73 +50,191 @@ async def _(event):
                 except:
                     bott = url.split('/')[-1]
                     await sedthon(ImportChatInviteRequest(bott))
-                msg2 = await sedthon.get_messages('@TRS04BOT', limit=1)
+                msg2 = await sedthon.get_messages('@zmmbot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
-                await sedthon.send_message(event.chat_id, f"تم الاشتراك في {chs} قناة")
+                await sedthon.send_message("me", f"تم الاشتراك في {chs} قناة")
             except:
-                await sedthon.send_message(event.chat_id, f"خطأ , ممكن تبندت(@JJXJXX)")
+                await sedthon.send_message(event.chat_id, f"خطأ , ممكن تبندت")
                 break
-        await sedthon.send_message(event.chat_id, "تم الانتهاء من التجميع !(@JJXJXX)")
+        await sedthon.send_message(event.chat_id, "تم الانتهاء من التجميع !")
 
     else:
         await event.edit("يجب الدفع لاستعمال هذا الامر !")
 
-
-@sedthon.on(events.NewMessage(incoming=True))
-async def handle_new_message(event):
-
-    if "stdno1" in event.message.text:
-
-        sender = await event.get_sender()
-        username = sender.username
-
-        await sedthon.send_message(event.chat_id, f"""
         
+#المليون
 
-    .تجميع المليار
-    
-    """)
+c = requests.session()
+bot_username = '@L5hbot'
+
+
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.بوت المليون"))
+async def _(event):
+    if ispay[0] == "yes":
+        await event.edit(t06bot)
+    else:
+        await event.edit("يجب الدفع لاستعمال هذا الامر !")
+
+
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع المليون"))
+async def _(event):
+    if ispay[0] == "yes":
+        await event.edit("**᯽︙سيتم تجميع النقاط , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+        channel_entity = await sedthon.get_entity(bot_username)
+        await sedthon.send_message('@L5hbot', '/start')
+        await asyncio.sleep(5)
+        msg0 = await sedthon.get_messages('@L5hbot', limit=1)
+        await msg0[0].click(2)
+        await asyncio.sleep(5)
+        msg1 = await sedthon.get_messages('@L5hbot', limit=1)
+        await msg1[0].click(0)
+
+        chs = 1
+        for i in range(100):
+            if ispay[0] == 'no':
+                break
+            await asyncio.sleep(5)
+
+            list = await sedthon(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+            msgs = list.messages[0]
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await sedthon.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
+                break
+            url = msgs.reply_markup.rows[0].buttons[0].url
+            try:
+                try:
+                    await sedthon(JoinChannelRequest(url))
+                except:
+                    bott = url.split('/')[-1]
+                    await sedthon(ImportChatInviteRequest(bott))
+                msg2 = await sedthon.get_messages('@L5hbot', limit=1)
+                await msg2[0].click(text='تحقق')
+                chs += 1
+                await sedthon.send_message("me", f"تم الاشتراك في {chs} قناة")
+            except:
+                await sedthon.send_message(event.chat_id, f"خطأ , ممكن تبندت")
+                break
+        await sedthon.send_message(event.chat_id, "تم الانتهاء من التجميع !")
+
+    else:
+        await event.edit("يجب الدفع لاستعمال هذا الامر !")
+
         
-@sedthon.on(events.NewMessage(incoming=True))
-async def handle_new_message(event):
+#الازرق
 
-    if "stdno2" in event.message.text:
+c = requests.session()
+bot_username = '@MARKTEBOT'
 
-        sender = await event.get_sender()
-        username = sender.username
 
-        await sedthon.send_message(event.chat_id, f"""
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.بوت الازرق"))
+async def _(event):
+    if ispay[0] == "yes":
+        await event.edit(t06bot)
+    else:
+        await event.edit("يجب الدفع لاستعمال هذا الامر !")
+
+
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع الازرق"))
+async def _(event):
+    if ispay[0] == "yes":
+        await event.edit("**᯽︙سيتم تجميع النقاط , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+        channel_entity = await sedthon.get_entity(bot_username)
+        await sedthon.send_message('@MARKTEBOT', '/start')
+        await asyncio.sleep(5)
+        msg0 = await sedthon.get_messages('@MARKTEBOT', limit=1)
+        await msg0[0].click(2)
+        await asyncio.sleep(5)
+        msg1 = await sedthon.get_messages('@MARKTEBOT', limit=1)
+        await msg1[0].click(0)
+
+        chs = 1
+        for i in range(100):
+            if ispay[0] == 'no':
+                break
+            await asyncio.sleep(5)
+
+            list = await sedthon(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+            msgs = list.messages[0]
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await sedthon.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
+                break
+            url = msgs.reply_markup.rows[0].buttons[0].url
+            try:
+                try:
+                    await sedthon(JoinChannelRequest(url))
+                except:
+                    bott = url.split('/')[-1]
+                    await sedthon(ImportChatInviteRequest(bott))
+                msg2 = await sedthon.get_messages('@MARKTEBOT', limit=1)
+                await msg2[0].click(text='تحقق')
+                chs += 1
+                await sedthon.send_message("me", f"تم الاشتراك في {chs} قناة")
+            except:
+                await sedthon.send_message(event.chat_id, f"خطأ , ممكن تبندت")
+                break
+        await sedthon.send_message(event.chat_id, "تم الانتهاء من التجميع !")
+
+    else:
+        await event.edit("يجب الدفع لاستعمال هذا الامر !")
         
-
-    .تجميع المليون
-    
-    """)
-
-@sedthon.on(events.NewMessage(incoming=True))
-async def handle_new_message(event):
-
-    if "stdno3" in event.message.text:
-
-        sender = await event.get_sender()
-        username = sender.username
-
-        await sedthon.send_message(event.chat_id, f"""
-
-      .تجميع الازرق
         
-""")
+#العرب
 
-@sedthon.on(events.NewMessage(incoming=True))
-async def handle_new_message(event):
+c = requests.session()
+bot_username = '@xnsex21bot'
 
-    if "stdno4" in event.message.text:
 
-        sender = await event.get_sender()
-        username = sender.username
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.بوت العرب"))
+async def _(event):
+    if ispay[0] == "yes":
+        await event.edit(t06bot)
+    else:
+        await event.edit("يجب الدفع لاستعمال هذا الامر !")
 
-        await sedthon.send_message(event.chat_id, f"""
 
-      .تجميع العرب   
-        
-""")
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع العرب"))
+async def _(event):
+    if ispay[0] == "yes":
+        await event.edit("**᯽︙سيتم تجميع النقاط , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+        channel_entity = await sedthon.get_entity(bot_username)
+        await sedthon.send_message('@xnsex21bot', '/start')
+        await asyncio.sleep(5)
+        msg0 = await sedthon.get_messages('@xnsex21bot', limit=1)
+        await msg0[0].click(2)
+        await asyncio.sleep(5)
+        msg1 = await sedthon.get_messages('@xnsex21bot', limit=1)
+        await msg1[0].click(0)
+
+        chs = 1
+        for i in range(100):
+            if ispay[0] == 'no':
+                break
+            await asyncio.sleep(5)
+
+            list = await sedthon(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+            msgs = list.messages[0]
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await sedthon.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
+                break
+            url = msgs.reply_markup.rows[0].buttons[0].url
+            try:
+                try:
+                    await sedthon(JoinChannelRequest(url))
+                except:
+                    bott = url.split('/')[-1]
+                    await sedthon(ImportChatInviteRequest(bott))
+                msg2 = await sedthon.get_messages('@xnsex21bot', limit=1)
+                await msg2[0].click(text='تحقق')
+                chs += 1
+                await sedthon.send_message("me", f"تم الاشتراك في {chs} قناة")
+            except:
+                await sedthon.send_message(event.chat_id, f"خطأ , ممكن تبندت")
+                break
+        await sedthon.send_message(event.chat_id, "تم الانتهاء من التجميع !")
+
+    else:
+        await event.edit("يجب الدفع لاستعمال هذا الامر !")
